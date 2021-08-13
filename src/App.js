e65705de7a34662ba84f2df5d82e20aa2d4fc3c8
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-13 17:14:02 -0400

    displayed beers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import Homepage from './components/Homepage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+
+
+    return (
+      <div className="App">   
+      <Homepage/>  
+      <Switch>
+         <Route exact path='/beers'/>
+         <Route exact path='/random-beer'/>
+         <Route exact path='/new-beer'/>
+      </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
