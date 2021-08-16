5a6732a21b7f3c69cb33c7ca11cc10e07b34dbc8
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-16 12:47:51 -0400

    added axios

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import HomeButton from './HomeButton';
+import axios from 'axios';
 
 class Beers extends Component {
+state = {
+    beers:[]
+}
+
+componentDidMount(){
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers`)
+    .then(response=>{
+        console.log(response.data)
+    this.setState({
+        beers: response.data
+    })
+    })
+}
+
+allBeers = () => {
+    return this.state.beers.map(eachBeer=>{
+        <div>
+            <h2>{eachBeer.name}</h2>
+        </div>
+    })
+}
+
+
+
+
     render() {
         return (
             <div>
